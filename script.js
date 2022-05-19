fd100ed3a4fb46f61a551501a7e19a71f2c1e39c
Colin Ho <colinho97@gmail.com>
2022-05-18 20:14:46 -0400

    added in basic HTML and JS code, which is the basis for the project design

---
+++ script.js
@@ -0,0 +1,54 @@
+// PSEUDO-CODE (JS):
+    // Landing page with a button with text "Test your Knowledge" with title "Pokemon Guessing Game"
+
+    // Get information from PokeApi
+        // Create function for randomizing number
+            // Set the upper limit of random number to length of the array based on generation (ie: Gen 1 relate to Pokemon #1-151)
+        // Call PokeApi
+            // Get random image from PokeApi using randomizer function
+            // Get name of Pokemon and store it in a variable
+            // Display image on web page
+    //App functionality
+        //Query select the 'form' element
+        //Add submit event listener
+        //Conditional statement for whether the submission is equal to the Pokemon name
+
+    
+
+
+
+
+
+const pokeApp = {}
+
+pokeApp.endpoint = 'https://pokeapi.co/api/v2/pokemon-form/';
+
+pokeApp.init = () => {
+    pokeApp.getPokemon();
+}
+
+pokeApp.getPokemon = () => {
+
+    //for below variable, need to include '/number' for Pokemon #
+    const pokemonUrl = new URL(`${pokeApp.endpoint}`);
+    
+    pokemonUrl.search = new URLSearchParams({
+        limit: 100000,
+        //any additional parameters (ie: generation, version)
+    })
+
+    fetch(pokemonUrl)
+        .then( (response) => {
+            return response.json();
+        })
+        .then( (jsonData) => {
+            pokeApp.displayImages(jsonData);
+        })
+}
+
+
+pokeApp.displayImages = (result) => {
+    console.log(result);
+}
+
+pokeApp.init();
